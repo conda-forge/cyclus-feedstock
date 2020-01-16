bc716254ae21629013785ade2b11c67d4886dfe7
Baptiste Mouginot <mouginot.baptiste@gmail.com>
2020-01-16 17:14:01 -0600

    restauring old build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,10 +4,11 @@ set -e
 if [ "$(uname)" == "Darwin" ]; then
   # other
   libext=".dylib"
-  export LDFLAGS=""#-rpath ${PREFIX}/lib ${LDFLAGS}"
+  export LDFLAGS=""
+  #-rpath ${PREFIX}/lib ${LDFLAGS}"
   #export LINKFLAGS="-t undefined dynamic_lookup ${LDFLAGS}"
   #export LDFLAGS="-t undefined dynamic_lookup ${LDFLAGS}"
-  skiprpath="-DCMAKE_SKIP_RPATH=TRUE"
+  #skiprpath="-DCMAKE_SKIP_RPATH=TRUE"
 else
   libext=".so"
   skiprpath=""
@@ -23,8 +24,7 @@ ${PYTHON} install.py --prefix="${PREFIX}" \
   --core-version="${PKG_VERSION}" \
   -DBLAS_LIBRARIES="-L${PREFIX}/lib -lopenblas" \
   -DLAPACK_LIBRARIES="-L${PREFIX}/lib -lopenblas" \
-  --dont-allow-milps \
   --clean -j "${CPU_COUNT}"
 #  ${skiprpath} \
+#  --dont-allow-milps \
 #  -DCMAKE_OSX_DEPLOYMENT_TARGET="${MACOSX_VERSION_MIN}" \
-
