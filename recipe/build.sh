87f650b254072877f55bf3ad00565f1846e4fda9
Baptiste Mouginot <mouginot.baptiste@gmail.com>
2020-01-16 17:06:17 -0600

    ?>????

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,7 +4,7 @@ set -e
 if [ "$(uname)" == "Darwin" ]; then
   # other
   libext=".dylib"
-  export LDFLAGS=" "#-rpath ${PREFIX}/lib ${LDFLAGS}"
+  export LDFLAGS=""#-rpath ${PREFIX}/lib ${LDFLAGS}"
   #export LINKFLAGS="-t undefined dynamic_lookup ${LDFLAGS}"
   #export LDFLAGS="-t undefined dynamic_lookup ${LDFLAGS}"
   skiprpath="-DCMAKE_SKIP_RPATH=TRUE"
