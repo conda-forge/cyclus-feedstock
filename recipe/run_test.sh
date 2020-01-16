998a8b7b424f7a6d6b780a85958788d88713602d
Baptiste Mouginot <mouginot.baptiste@gmail.com>
2020-01-16 09:58:51 -0600

    removing PATH setup

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 # setup env for tests
 cd tests
-export PATH="${PREFIX}/bin:${PATH}"
-export CYCLUS_PATH="${PREFIX}/lib/cyclus"
-if [ -z "$CYCLUS_NUC_DATA" ]; then
-  export CYCLUS_NUC_DATA="${PREFIX}/share/cyclus/cyclus_nuc_data.h5"
-fi
-
-UNAME="$(uname)"
-if [ "${UNAME}" == "Darwin" ]; then
-  export DYLD_FALLBACK_LIBRARY_PATH="${PREFIX}/lib/cyclus:${PREFIX}/lib:${DYLD_FALLBACK_LIBRARY_PATH}"
-else
-  export LD_LIBRARY_PATH="${PREFIX}/lib/cyclus:${PREFIX}/lib:${LD_LIBRARY_PATH}"
-fi
+#export PATH="${PREFIX}/bin:${PATH}"
+#export CYCLUS_PATH="${PREFIX}/lib/cyclus"
+#if [ -z "$CYCLUS_NUC_DATA" ]; then
+#  export CYCLUS_NUC_DATA="${PREFIX}/share/cyclus/cyclus_nuc_data.h5"
+#fi
+#
+#UNAME="$(uname)"
+#if [ "${UNAME}" == "Darwin" ]; then
+#  export DYLD_FALLBACK_LIBRARY_PATH="${PREFIX}/lib/cyclus:${PREFIX}/lib:${DYLD_FALLBACK_LIBRARY_PATH}"
+#else
+#  export LD_LIBRARY_PATH="${PREFIX}/lib/cyclus:${PREFIX}/lib:${LD_LIBRARY_PATH}"
+#fi
 
 # check that the files exist
 test -f ${PREFIX}/bin/cyclus
