cd36183b2b108549cac85e4bdcb85ca0f73dcf67
Baptiste Mouginot <mouginot.baptiste@gmail.com>
2020-01-16 12:14:40 -0600

    remove rpathskip flag

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,7 +6,7 @@ set -e
 #  libext=".dylib"
 #  #export LDFLAGS="-rpath ${PREFIX}/lib ${LDFLAGS}"
 #  export LINKFLAGS="${LDFLAGS}"
-skiprpath="-DCMAKE_SKIP_RPATH=TRUE"
+#skiprpath="-DCMAKE_SKIP_RPATH=TRUE"
 #else
 #  libext=".so"
 #  skiprpath=""
@@ -22,7 +22,7 @@ ${PYTHON} install.py --prefix="${PREFIX}" \
   --core-version="${PKG_VERSION}" \
   -DBLAS_LIBRARIES="-L${PREFIX}/lib -lopenblas" \
   -DLAPACK_LIBRARIES="-L${PREFIX}/lib -lopenblas" \
-  ${skiprpath} \
   --clean -j "${CPU_COUNT}"
+#  ${skiprpath} \
 #  --dont-allow-milps \
 #  -DCMAKE_OSX_DEPLOYMENT_TARGET="${MACOSX_VERSION_MIN}" \
