fad95e5c7312d2123116664df2887460c8b4963c
Baptiste Mouginot <mouginot.baptiste@gmail.com>
2020-01-16 11:03:00 -0600

    all the same

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 set -e
 
-if [ "$(uname)" == "Darwin" ]; then
-  # other
-  libext=".dylib"
-  #export LDFLAGS="-rpath ${PREFIX}/lib ${LDFLAGS}"
-  export LINKFLAGS="${LDFLAGS}"
-  skiprpath="-DCMAKE_SKIP_RPATH=TRUE"
-else
-  libext=".so"
-  skiprpath=""
-fi
+#if [ "$(uname)" == "Darwin" ]; then
+#  # other
+#  libext=".dylib"
+#  #export LDFLAGS="-rpath ${PREFIX}/lib ${LDFLAGS}"
+#  export LINKFLAGS="${LDFLAGS}"
+#  skiprpath="-DCMAKE_SKIP_RPATH=TRUE"
+#else
+#  libext=".so"
+#  skiprpath=""
+#fi
 
 
 
@@ -24,5 +24,5 @@ ${PYTHON} install.py --prefix="${PREFIX}" \
   -DCMAKE_OSX_DEPLOYMENT_TARGET="${MACOSX_VERSION_MIN}" \
   -DBLAS_LIBRARIES="-L${PREFIX}/lib -lopenblas" \
   -DLAPACK_LIBRARIES="-L${PREFIX}/lib -lopenblas" \
-  ${skiprpath} \
   --clean -j "${CPU_COUNT}"
+#  ${skiprpath} \
