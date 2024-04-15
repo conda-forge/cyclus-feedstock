0500baf2c19b46f83f8a4c4a5da7ae0fbd98bba7
Ben Nibbelink <bnibbs@vt.edu>
2024-04-15 14:30:24 -0500

    some updates on latest commit

--- recipe/build.sh
+++ recipe/build.sh
@@ -16,11 +16,6 @@ fi
 #export VERBOSE=1
 ${PYTHON} install.py --prefix="${PREFIX}" \
   --build_type="Release" \
-  --dont-allow-milps \
-  --deps-root="${PREFIX}" \
-  --core-version="${PKG_VERSION}" \
-  -DCMAKE_OSX_DEPLOYMENT_TARGET="${MACOSX_VERSION_MIN}" \
-  -DBLAS_LIBRARIES="-L${PREFIX}/lib -lopenblas" \
-  -DLAPACK_LIBRARIES="-L${PREFIX}/lib -lopenblas" \
+  --allow-milps \
   ${skiprpath} \
-  --clean -j "${CPU_COUNT}"
+  --clean-build -j "${CPU_COUNT}"
