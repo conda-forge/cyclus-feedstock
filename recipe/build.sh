d7d662554c7916f9539166053953e3eef0fbdd1d
bennibbelink <79653949+bennibbelink@users.noreply.github.com>
2024-10-31 21:05:15 -0400

    clean up build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,20 +1,11 @@
 #!/usr/bin/env bash
 set -e
 
-if [ "$(uname)" == "Darwin" ]; then
-  # FIND_FRAMEWORK_NEVER="-DCMAKE_FIND_FRAMEWORK=NEVER"
-  # FIND_APPBUNDLE_NEVER="-DCMAKE_FIND_APPBUNDLE=NEVER"
-  BLAS_HINT="-DBLAS_ROOT=${CONDA_PREFIX}" # Tell CMake to use conda BLAS and not system BlAS
-fi
-
 # Install Cyclus
 #export VERBOSE=1
 ${PYTHON} install.py \
   --prefix=${PREFIX} \
   ${CMAKE_ARGS} \
-  # ${FIND_FRAMEWORK_NEVER} \
-  # ${FIND_APPBUNDLE_NEVER} \
-  ${BLAS_HINT} \
   --allow-milps \
   -DCMAKE_C_COMPILER=${CC} \
   -DCMAKE_CXX_COMPILER=${CXX} \
